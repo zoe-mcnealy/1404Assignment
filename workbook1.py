5b14cadac33d5ac35f4e017a87cf853686df73b2
zoemcnealy <zoe.mcnealy@my.jcu.edu.au>
2016-08-28 20:10:01 +1000

    Started the for loop for formatting the data in the CSV file

--- workbook1.py
+++ workbook1.py
@@ -1,1 +1,23 @@
-"try again"
+infile = open("items.csv","r")
+items_list = []
+shopping_items= []
+item_costs = []
+priority_num = []
+is_completed = []
+
+for line in infile:
+    line= line.strip("\n")
+    items_list = line.split(',')
+    shopping_items.append(items_list[0])
+    item_costs.append(items_list[1])
+    priority_num.append(items_list[2])
+    is_completed.append(items_list[3])
+
+print(shopping_items)
+print(item_costs)
+print(priority_num)
+print(is_completed)
+
+for i in range(0,len(shopping_items)):
+    print("On your Shopping list there is:\n {} for ${:.5}. It is number {} on your list".format(shopping_items[i],item_costs[i],priority_num[i]))
+
