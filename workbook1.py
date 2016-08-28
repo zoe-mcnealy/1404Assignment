57ee8995646fe7a8247b3a13f90b08b199c7ec7b
zoemcnealy <zoe.mcnealy@my.jcu.edu.au>
2016-08-28 20:44:07 +1000

    The "list items" function is now working

--- workbook1.py
+++ workbook1.py
@@ -10,31 +10,31 @@ MENU = "Please Choose and Option:\nR - List required items\nC - List completed I
 print(MENU)
 choice = input("").upper()
 
-#while choice != "Q":
-if choice == "R":
-    infile = open("items.csv","r")
-    for line in infile:
-        line= line.strip("\n")
-        items_list = line.split(',')
-        shopping_items.append(items_list[0])
-        item_costs.append(items_list[1])
-        priority_num.append(items_list[2])
-        is_completed.append(items_list[3])
-#print(shopping_items)
-#print(item_costs)
-#print(priority_num)
-#print(is_completed)
-    for i in range(0,len(shopping_items)):
-        print(i+1 ,". {}\t ${:.5} ({})".format(shopping_items[i],item_costs[i],priority_num[i]))
-elif choice == "C":
-    print("TO DO")
-elif choice == "A":
-    print("TO DO")
-elif choice == "M":
-    print("TO DO")
-else:
-        print("Invalid Choice. Please choose again.")
-        choice = input("R - List required items\n C - List completed Items\n A - Add a new item\n Q - Quit")
+while choice != "Q":
+    if choice == "R":
+        infile = open("items.csv","r")
+        for line in infile:
+            line= line.strip("\n")
+            items_list = line.split(',')
+            shopping_items.append(items_list[0])
+            item_costs.append(items_list[1])
+            priority_num.append(items_list[2])
+            is_completed.append(items_list[3])
+    #print(shopping_items)
+    #print(item_costs)
+    #print(priority_num)
+    #print(is_completed)
+        for i in range(0,len(shopping_items)):
+            print(i+1 ,". {}\t ${:.5} ({})".format(shopping_items[i],item_costs[i],priority_num[i]))
+    elif choice == "C":
+        print("TO DO")
+    elif choice == "A":
+        print("TO DO")
+    elif choice == "M":
+        print("TO DO")
+    else:
+            print("Invalid Choice. Please choose again.")
+            choice = input("R - List required items\n C - List completed Items\n A - Add a new item\n Q - Quit")
 
 
 
