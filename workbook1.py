2b7be257662ea2c74bfa01232e41bfe74dae2009
zoemcnealy <zoe.mcnealy@my.jcu.edu.au>
2016-09-01 13:24:35 +1000

    started to write in functions f

--- workbook1.py
+++ workbook1.py
@@ -5,6 +5,8 @@ shopping_items= []
 item_costs = []
 priority_num = []
 is_completed = []
+total_cost = 0
+num_items = 0
 
 MENU = "Please Choose and Option:\nR - List required items\nC - List completed Items\nA - Add a new item\nM - Mark an item as completed\nQ - Quit"
 print(MENU)
@@ -12,6 +14,7 @@ choice = input("").upper()
 
 while choice != "Q":
     if choice == "R":
+
         infile = open("items.csv","r")
         for line in infile:
             line= line.strip("\n")
@@ -22,11 +25,9 @@ while choice != "Q":
             is_completed.append(items_list[3])
         infile.close()
         print("Required Items:")
-        total_cost = 0
-        num_items = 0
         for i in range(0,len(shopping_items)):
             if is_completed[i] == "r":
-                print(i+1,". {}\t ${:.2f} ({})".format(shopping_items[i],item_costs[i],priority_num[i]))
+                print(1,". {}\t ${:.2f} ({})".format(shopping_items[i],item_costs[i],priority_num[i]))
                 total_cost = total_cost + item_costs[i]
                 num_items = num_items + 1
 
@@ -43,11 +44,11 @@ while choice != "Q":
             priority_num.append(int(items_list[2]))
             is_completed.append(items_list[3])
         infile.close()
-        print("Required Items:")
+        print("Completed Items:")
         total_cost = 0
         for i in range(0, len(shopping_items)):
-            if is_completed[i] == "r":
-                print(i + 1, ". {:15s} ${:6.2f} ({})".format(shopping_items[i], item_costs[i], priority_num[i]))
+            if is_completed[i] == "c":
+                print(i, ". {:15s} ${:6.2f} ({})".format(shopping_items[i], item_costs[i], priority_num[i]))
                 total_cost = total_cost + item_costs[i]
                 num_items = num_items + 1
 
